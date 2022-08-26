3cdf80d2e709a12d3bb056e9f421b23d0d695b48
Ryanne-py <roma.kostik12@gmail.com>
2022-08-26 21:02:21 +0300

    Added the function to automatically set the date of editing after editing the post

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,17 +1,33 @@
-from .serializers import RubricSerializer, PostSerializer
+from django.utils import timezone
+from .serializers import RubricSerializer, PostSerializer, PostEditSerializer
 import post.services as services
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class PostListCreate(generics.ListCreateAPIView):
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return services.get_post()
 
 
+class PostEdit(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostEditSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return services.get_post()
+
+    def perform_update(self, serializer):
+        serializer.instance.post_edit_date = timezone.now()
+        serializer.save()
+        services.update_post_edit_date(serializer)
+
+
 class RubricListCreate(generics.ListCreateAPIView):
     serializer_class = RubricSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return services.get_rubric()
