04dab7589c1285084b60800f49e3b004a064b2f6
Ryanne-py <roma.kostik12@gmail.com>
2022-08-28 18:00:07 +0300

    Added cors origin white list

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
