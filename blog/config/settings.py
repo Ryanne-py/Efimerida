7c0c9fc0bf1d82f8d4b192233a0ab3c009717e3b
Ryanne-py <roma.kostik12@gmail.com>
2022-09-10 20:01:12 +0300

    Added settings and paths to images

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'corsheaders',
     'post',
 ]
-
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -120,6 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
