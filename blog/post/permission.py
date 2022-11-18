278169bb0dd0320dd943bab6f7ccd6d6ae00fb63
Ryanne-py <roma.kostik12@gmail.com>
2022-11-18 20:43:47 +0200

    add view(PostListByFilter, Post, TagList, TagDetail) and other detail for this .Little bit Optimization old servise

--- blog/post/permission.py
+++ blog/post/permission.py
@@ -2,6 +2,9 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Allows access only to post owner, or is a read-only request.
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -6,6 +6,8 @@
 Welcome to Efimerida's documentation!
 =====================================
 
+
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
