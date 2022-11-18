0ffe73a76ea1af16a9870272958c83b77bac2ff2
Ryanne-py <roma.kostik12@gmail.com>
2022-11-18 20:28:42 +0200

    add nue view(PostListByFilter, Post, TagList, TagDetail).Little bit Optimization old view

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,7 +1,7 @@
 from .models import CustomUser
 from .serializers import *
-import post.services as services
-from rest_framework import generics, permissions, viewsets
+from post.services import *
+from rest_framework import generics, permissions, views
 from rest_framework.response import Response
 from django.db import IntegrityError
 from django.http import JsonResponse
@@ -13,38 +13,44 @@ import post.generics as additional_generics
 @csrf_exempt
 def registration(request):
     if request.method == 'POST':
-        token = services.registration(request)
+        token = UserServices.registration(request)
         return token
 
 
 @csrf_exempt
 def authentication(request):
     if request.method == 'POST':
-        token = services.authentication(request)
+        token = UserServices.authentication(request)
         return token
 
 
 class UserList(generics.ListAPIView):
-    queryset = services.get_user_list()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return UserServices.get_user_list()
 
     def __str__(self):
         return 'UserListView'
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = services.get_user_list()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return UserServices.get_user_list()
 
     def __str__(self):
         return 'UserDetailView'
 
 
 class PostList(generics.ListCreateAPIView):
-    queryset = services.get_post_list(serializer=False)
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return PostServices.get_post_list()
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
@@ -60,17 +66,19 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = services.get_post_list(serializer=False)
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+    def get_queryset(self):
+        return PostServices.get_post_list()
+
     def perform_update(self, serializer):
-        services.update_post_edit_date(serializer)
+        PostServisec.update_post_edit_date(serializer)
         serializer.save()
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-        instance = services.add_post_view(instance)
+        instance = PostServices.add_post_view(instance)
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
@@ -78,9 +86,31 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
         return 'PostDetailView'
 
 
+class PostListByFilter(additional_generics.PostListWithFilterAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        return PostServices.get_post_list()
+
+
+class PostLike(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, **kwargs):
+        detail, status = PostServices.like_post(request.user, kwargs['pk'])
+        return Response(detail, status=status)
+
+    def delete(self, request, **kwargs):
+        detail, status = PostServices.cancel_like(request.user, kwargs['pk'])
+        return Response(detail, status=status)
+
+
 class CommentList(generics.ListCreateAPIView):
-    queryset = services.get_comment_list()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return CommentServices.get_comment_list()
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
@@ -89,7 +119,8 @@ class CommentList(generics.ListCreateAPIView):
             return CommentSerializer
 
     def list(self, request, *args, **kwargs):
-        return Response(services.get_comment_on_post(self, request, *args, **kwargs))
+        serializer, status = CommentServices.get_comment_on_post(self, kwargs['pk'])
+        return Response(serializer.data, status=status)
 
     def perform_create(self, serializer):
         serializer.save(comment_author=self.request.user)
@@ -99,27 +130,62 @@ class CommentList(generics.ListCreateAPIView):
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = services.get_comment_list()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return CommentServices.get_comment_list()
 
     def __str__(self):
         return 'CommentDetailView'
 
 
-class RubricList(generics.ListAPIView):
+class RubricList(generics.ListCreateAPIView):
     serializer_class = RubricSerializer
-    queryset = services.get_rubric()
-    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        return RubricServices.get_rubric()
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            permission_classes = [permissions.IsAdminUser]
+        elif self.request.method == 'GET':
+            permission_classes = [permissions.AllowAny]
+
+        return [permission() for permission in permission_classes]
 
     def __str__(self):
         return 'RubricListView'
 
 
-class RubricDetail(additional_generics.CreateUpdateDestroyAPIView):
+class RubricDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RubricSerializer
-    queryset = services.get_rubric()
     permission_classes = [permissions.IsAdminUser]
+
+    def get_queryset(self):
+        return RubricServices.get_rubric()
 
     def __str__(self):
         return 'RubricDetailView'
+
+
+class TagList(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return TagServices.get_tags()
+
+    def __str__(self):
+        return 'TagListView'
+
+
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return TagServices.get_tags()
+
+    def __str__(self):
+        return 'TagDetailView'
